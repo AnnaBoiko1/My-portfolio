214fe5a2c7402b087d91ac3a9b5c47a8b4c37738
Anna Boiko <annaboiko1@icloud.com>
2026-01-16 12:33:27 +0100

    chore: update dependencies and add PostCSS configuration
    - Added new dependencies: @emotion/react, @emotion/styled, @mui/material, and autoprefixer.
    - Created a new PostCSS configuration file to include Tailwind CSS and autoprefixer plugins.

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+export default function Home() {
+  const [value, setValue] = React.useState(0);
+  return (
+    <Box sx={{ width: 500, bottom: 0, position: "fixed" }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Home" href='/' />
+        <BottomNavigationAction label="About" href='/about' />
+        <BottomNavigationAction label="Projects" href='/projects' />
+        <BottomNavigationAction label="Contact" href='/contact'/>
+      </BottomNavigation>
+    </Box>
+  );
+}
